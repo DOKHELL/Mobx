07337d6e1af8236701f82e01ab53c5e92766bfd1
DOKHELL <48280426+DOKHELL@users.noreply.github.com>
2020-01-15 17:31:30 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {inject, observer} from "mobx-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@inject('store')
+@observer
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <span>{this.props.store.counter}</span><br/>
+            <button onClick={this.props.store.Increment}>+1</button>
+            <button onClick={this.props.store.Decrement}>-1</button>
+        </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
